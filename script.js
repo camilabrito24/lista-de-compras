1e8fecd40f5a3b5318b694a398dccaad2ddbbb75
Camila Brito <camilam.brito@outlook.com.br>
2024-10-21 22:17:36 -0300

    Ajustando interface e criando interação com checkbox.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const item = document.querySelector("#input-item");
 const btnSalvarItem = document.querySelector("#btn-adicionar-item");
 const listaDeCompras = document.querySelector("#lista-de-compras");
+const listaDeComprados = document.querySelector("#lista-comprados");
 let contador = 0;
 
 btnSalvarItem.addEventListener("click",adicionarItem);
@@ -11,7 +12,7 @@ function adicionarItem(evento){
     const containerItemLista = document.createElement("div");
     containerItemLista.classList.add("item-lista-container");
 
-    const containerNomeItem = document.createElement("div");
+    //const containerNomeItem = document.createElement("div");
     const containerCheckbox = document.createElement("div");
     containerCheckbox.classList.add("checkbox-container");
     const inputCheckbox = document.createElement("input");
@@ -22,17 +23,35 @@ function adicionarItem(evento){
     const labelCheckbox = document.createElement("label");
     labelCheckbox.setAttribute("for",inputCheckbox.id);
 
+    labelCheckbox.addEventListener("click", function (evento){
+        const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
+
+        if(checkboxInput.checked){
+            checkboxCustomizado.classList.add("checked");
+            listaDeComprados.appendChild(itemLista);
+            itemTitulo.style.textDecoration = "line-through";
+        } else{
+            checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemLista);
+        }
+
+    })
+
     const checkboxCustomizado  = document.createElement("div");
     checkboxCustomizado.classList.add("checkbox-customizado");
 
     labelCheckbox.appendChild(inputCheckbox);
     labelCheckbox.appendChild(checkboxCustomizado);
     containerCheckbox.appendChild(labelCheckbox);
-    containerNomeItem.appendChild(containerCheckbox);
+    containerItemLista.appendChild(containerCheckbox);
 
     const nomeItem = document.createElement("p");
+    nomeItem.id = "item-titulo";
     nomeItem.innerText = item.value;
-    containerNomeItem.appendChild(nomeItem);
+    containerItemLista.appendChild(nomeItem);
 
     const containerBtn = document.createElement("div");
     const btnRemover = document.createElement("button");
@@ -53,7 +72,7 @@ function adicionarItem(evento){
     btnEditar.appendChild(imagemEditar);
     containerBtn.appendChild(btnEditar);
 
-    containerItemLista.appendChild(containerNomeItem);
+    //containerItemLista.appendChild(containerNomeItem);
     containerItemLista.appendChild(containerBtn);
     itemLista.appendChild(containerItemLista);
     listaDeCompras.appendChild(itemLista);
